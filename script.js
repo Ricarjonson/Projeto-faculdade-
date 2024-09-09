34f8451994c3ea8ee2ec3350dc99b723b0d6243e
Ricarjonson <165069386+Ricarjonson@users.noreply.github.com>
2024-09-09 20:15:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+        // Validações de formulário
+        document.getElementById('cadastroForm').addEventListener('submit', function(event) 
+            event.preventDefault();
+            var nomeCliente = document.getElementById('nomeCliente').value;
+            var emailCliente = document.getElementById('emailCliente')
+              </script>
+
+
+  
